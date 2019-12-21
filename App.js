6a47dee3a6ee6a3c8f79538ce00710830782250a
tmaulon <thomas.maulon@gmail.com>
2019-12-21 14:05:05 +0100

    add swipe left, right and reset animation on fact card

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Animated, PanResponder } from "react-native";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import axios from "axios";
 
-import FactCard from './components/fact-card'
+import FactCard from './components/fact-card';
 
+// a quoi ca doit ressembler ? https://giphy.com/gifs/animation-illustration-motion-26mkhMYkitO7DoJuU/fullscreen
+const RANDOM_FACT_URL =
+  "http://randomuselessfact.appspot.com/random.json?language=en";
+const PICTURE_LIST_URL = `https://picsum.photos/${hp("30%")}/${hp("90%")}?image=`;
+const MAX_LEFT_ROTATION_DISTANCE = wp("-150%");
+const MAX_RIGHT_ROTATION_DISTANCE = wp("150%");
+const LEFT_TRESHOLD_BEFORE_SWIPE = wp("-50%");
+const RIGHT_TRESHOLD_BEFORE_SWIPE = wp("50%");
 export default class App extends Component {
 
   constructor(props) {
@@ -15,18 +25,45 @@ export default class App extends Component {
     const panResponder = PanResponder.create({
       onMoveShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
-
         this.position.setValue({
           x: gesture.dx,
           y: parseFloat(JSON.stringify(this.position.y))
-        })
+        });
+      },
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx < LEFT_TRESHOLD_BEFORE_SWIPE) {
+          this.forceLeftExit();
+        } else if (gesture.dx > RIGHT_TRESHOLD_BEFORE_SWIPE) {
+          this.forceRightExit();
+        } else {
+          this.resetPositionSoft();
+        }
       }
     });
     this.setState({ panResponder });
   }
+
+  forceLeftExit() {
+    Animated.timing(this.position, {
+      toValue: { x: wp("-100%"), y: 0 }
+    }).start();
+  }
+  forceRightExit() {
+    Animated.timing(this.position, {
+      toValue: { x: wp("100%"), y: 0 }
+    }).start();
+  }
+  resetPositionSoft() {
+    Animated.spring(this.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
+
   getCardStyle() {
     const rotation = this.position.x.interpolate({
-      inputRange: [-200, 0, 200],
+      inputRange: [MAX_LEFT_ROTATION_DISTANCE, 0, MAX_RIGHT_ROTATION_DISTANCE
+      ],
       outputRange: ["-120deg", "0deg", "120deg"]
     })
     return {
